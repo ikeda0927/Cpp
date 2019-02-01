ca34f1abaa4578c597b827b3a33abd6eeb637a33
池田康平 <ikedakouhei@IKEDAPro.local>
2019-02-02 04:01:03 +0900

    Class Test1

--- ClassTest1.cpp
+++ ClassTest1.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 using namespace std;
 
-class Sushi{
+typedef class Sushi{
 public:
 	string name;
+	int amount;
 	int value;
-	int amount
+
+	Sushi(string name,int amount,int value);
+
+	void show();
+}Sushi;
+
+Sushi::Sushi(string name,int amount,int value){
+	this->name = name;
+	this->amount = amount;
+	this->value = value;
+}
+
+void Sushi::show(){
+	cout << name << "は" << amount << "貫で" << value << "円です。" << endl;
 }
 int main(){
-
+	Sushi sushi = Sushi("マグロ",2,100);
+	sushi.show();
 }
