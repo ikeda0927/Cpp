911c53cfb189ceedab5d9ad06f390089fb757f20
池田康平 <ikedakouhei@IKEDAPro.local>
2019-02-12 23:14:27 +0900

    add All mode

--- CeasarCipher3.cpp
+++ CeasarCipher3.cpp
@@ -13,8 +13,9 @@ int numWeight = 2;
 size_t StrLen(char* str);//入力した文字列の長さを返す関数のプロトタイプ宣言
 char replace(char c);
 bool isLoop = true;
+bool isCheckAll=false;
 int main(int argc, char* argv[]){
-	if(argc > 1 && *argv[1] != 'r'){//コマンドライン引数の二番目をweightとして受け取る
+	if(argc > 1 && *argv[1] != 'r' && *argv[1] != 'a'){//コマンドライン引数の二番目をweightとして受け取る
 		weight = atoi(argv[1]);//コマンドライン引数から受け取ったものはchar*なのでintにする。
 		if(argc > 2 && *argv[2] == 'r'){
 			alpWeight = weight;
@@ -47,22 +48,42 @@ int main(int argc, char* argv[]){
 		while(numWeight >= NUM_SUM){
 			numWeight-=NUM_SUM;
 		}
+	}else if(argc > 1 && *argv[1] == 'a'){
+		isCheckAll=true;
+		cout << "isCheckAll" << endl;
 	}
+	// cout << "argv[1] : " << *argv[1] << endl;
 	while(isLoop){
 		cout << "Enter string.(Use '_' not ' ')" << endl;
 		char s1[512];//十分な大きさの配列を用意
 		cin >> s1;//文字列を読み込む
-		char* s1p = s1;//読み込んだ文字列のポインタをセット
-		//int stringLength = (int)StrLen(s1);//文字列の長さをゲット
-		//while(stringLength>0){//大体真ん中らへんに矢印を持っていきたい
-		//	stringLength-=2;
-		//	cout << " " << flush;
-		//}
-		//cout << "↓ " << weight << flush;//矢印の隣にずらした数を記す
-		for(;*s1p != '\0';++s1p){//s1pの先頭アドレスにある文字が終端文字になるまで先頭アドレスをずらす
-			*s1p = replace(*s1p);
+		if(isCheckAll){
+			numWeight=1;
+			alpWeight=1;
+			for(int k=0;k<ALP_SUM*NUM_SUM/2;k++){
+				cout << "weight : " << k << endl;
+				// if(numWeight>NUM_SUM){
+				// 	numWeight=0;
+				// }
+				char* s1p = s1;//読み込んだ文字列のポインタをセット
+				for(;*s1p != '\0';++s1p){//s1pの先頭アドレスにある文字が終端文字になるまで先頭アドレスをずらす
+					*s1p = replace(*s1p);
+				}
+				cout << endl << s1 << endl;
+			}
+		}else{
+			char* s1p = s1;//読み込んだ文字列のポインタをセット
+			//int stringLength = (int)StrLen(s1);//文字列の長さをゲット
+			//while(stringLength>0){//大体真ん中らへんに矢印を持っていきたい
+			//	stringLength-=2;
+			//	cout << " " << flush;
+			//}
+			//cout << "↓ " << weight << flush;//矢印の隣にずらした数を記す
+			for(;*s1p != '\0';++s1p){//s1pの先頭アドレスにある文字が終端文字になるまで先頭アドレスをずらす
+				*s1p = replace(*s1p);
+			}
+			cout << endl << s1 << endl;
 		}
-		cout << endl << s1 << endl;
 		cout << "Continue? y/n" << endl;
 		char loop;
 		cin >> loop;
@@ -111,4 +132,4 @@ char replace(char c){
 //		//nop
 //	}
 //	return i;
-//}Stydgwetwdjeqyiygjgvchfreuppryjfjgv
+//}
